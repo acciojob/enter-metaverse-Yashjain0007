ebd5cb59eb1b5ae1212dcb1bcec657316d84d7d6
Yash Jain <124593014+Yashjain0007@users.noreply.github.com>
2023-05-19 17:16:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p = document.getElementById("status");
+function change() {
+	p.style.display = "none";
+	let newElement = document.createElement("h1");
+	newElement.innerText = "Entered Metaverse";
+	p.appendChild(newElement);
+}
